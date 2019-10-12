18756ce0307991749132f1c4f36eca40704cb02b
Shelby England <Shelby.England@knights.ucf.edu>
2019-10-12 01:43:50 -0400

    updating files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,80 +1,168 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-let thePlayer
-let theDefender
-let choosePlayer = false;
-let chooseDefender = false;
+    let thePlayer
+    let theDefender
 
-let korra = {
-"name" : "Korra",
-"hp" : 150,
-"ap" : 12,
-"cap" : 6,
-}
-
-let mako = {
-    "name" : "Mako",
-    "hp" : 130,
-    "ap" : 8,
-    "cap" : 4,
-}
-
-let asami = {
-    "name" : "Asami",
-    "hp" : 110,
-    "ap" : 6,
-    "cap" : 3,
-}
-
-let bolin = {
-    "name" : "Bolin",
-    "hp" : 140,
-    "ap" : 10,
-    "cap" : 5
-}
-
-
-
-
-console.log(korra)
-console.log(mako)
-console.log(asami)
-console.log(bolin)
-
-
-
-    $(".image").on("click" , function(){
-
-        
-
-    })
-
-
-
-//attack function
-    $("#attk-button").on("click", function() {
-
-        thePlayer = 
-        theDefender =    
-
-        theDefender.hp = theDefender.hp - thePlayer.ap
-        thePlayer.hp = thePlayer.hp - theDefender.cap
-        $("#fightingDiv").html(korra.image)
-
-        thePlayer.ap = thePlayer.ap + thePlayer.ap       
-
-        if (thePlayer.hp <= 0) {
-            console.log("game over")
-            //trigger reset function        
+    let players = {
+        korra: {
+            "name": "Korra",
+            "ability": "The Avatar",
+            "hp": 150,
+            "ap": 6,
+            "cap": 6
+        },
+        mako: {
+            "name": "Mako",
+            "ability": "Fire Bending",
+            "hp": 130,
+            "ap": 4,
+            "cap": 4
+        },
+        asami: {
+            "name": "Asami",
+            "ability" : "Intelligence",
+            "hp": 110,
+            "ap": 3,
+            "cap": 3
+        },
+        bolin: {
+            "name": "Bolin",
+            "ability": "Earth Bending",
+            "hp": 140,
+            "ap": 5,
+            "cap": 5
         }
-        if (theDefender.hp <= 0) {
-            //choose another player from the opponent div
-            console.log("pick another opponent")
-        }   
+    }
+
+
+    $(".image").on("click", function () {
+        if(thePlayer === $(this).attr("value") || theDefender === $(this).attr("value")) return false;
+        
+        if (!thePlayer) {
+            //move player to your-player section
+            //set choosePlayer to true
+            thePlayer = $(this).attr("value");
+            $("#your-player").append($(this));
+            $("#playerHP").html(`${players[thePlayer].name} HP is ${players[thePlayer].hp}`)
+            
+        } else {
+            //move player to defender section
+            //move remaining players to opponents section
+            theDefender = $(this).attr("value");
+            $("#defender").append($(this));
+            $("#players").children().each(function(){
+                $(this).appendTo($("#opponents"));
+            $("#defenderHP").html(`${players[theDefender].name} HP is ${players[theDefender].hp}`) 
+        
+            })
+        }
     })
 
 
+    //attack function
+    $("#attk-button").on("click", function () {
+
+        console.log(players[theDefender].hp)
+        // console.log(theDefender[hp])
+
+       
+
+        players[theDefender].hp = players[theDefender].hp - players[thePlayer].ap
+        console.log(`defender hp is ${players[theDefender].hp}`)
+
+        players[thePlayer].hp = players[thePlayer].hp - players[theDefender].cap
+        console.log(`player hp is ${players[thePlayer].hp}`)
+
+        $("#theInfo").html(`${players[thePlayer].name} attacked ${players[theDefender].name} and did ${players[thePlayer].ap} ap worth of damage.`)
+
+        $("#playerHP").html(`${players[thePlayer].name}'s HP is now ${players[thePlayer].hp}`)
+        $("#defenderHP").html(`${players[theDefender].name}'s HP is now ${players[theDefender].hp}`)        
+
+
+        players[thePlayer].ap = players[thePlayer].ap + players[thePlayer].cap
+        console.log(`player ap is ${players[thePlayer].ap}`)
+
+
+        if ($("#defender").children().length === 3) {
+            $("#defender").children().hide()
+            Swal.fire(
+                'You Won!',
+              )         
+            theRestart()
+        }
+
+        if (players[theDefender].hp <= 0) {
+            //choose another player from the opponent div
+            $("#defenderHP").html(`${players[theDefender].name} is dead`)
+            $("#defender").children().hide();
+            theDefender = "";
+
+        }
+
+        if (players[thePlayer].hp <= 0) {
+            Swal.fire(
+                'You Died!',
+              )    
+            theRestart()       
+        } // if statement end bracket
+
+
+
+      
+        
+    
+    }) // attk-button on.click end bracket
+
+function theRestart() {
+    console.log("Restart")
+    
+    $("#your-player").children().each(function(){
+        $(this).appendTo($("#players"))
+    })
+
+    $("#defender").children().show().each(function(){
+        $(this).appendTo($("#players"))
+    })
+
+    $("#theInfo").empty()
+    $("#playerHP").empty()
+    $("#defenderHP").empty()
+
+    thePlayer = ""
+    theDefender = ""
+
+    players = {
+        korra: {
+            "name": "Korra",
+            "ability": "The Avatar",
+            "hp": 150,
+            "ap": 6,
+            "cap": 6
+        },
+        mako: {
+            "name": "Mako",
+            "ability": "Fire Bending",
+            "hp": 130,
+            "ap": 4,
+            "cap": 4
+        },
+        asami: {
+            "name": "Asami",
+            "ability" : "Intelligence",
+            "hp": 110,
+            "ap": 3,
+            "cap": 3
+        },
+        bolin: {
+            "name": "Bolin",
+            "ability": "Earth Bending",
+            "hp": 140,
+            "ap": 5,
+            "cap": 5
+        }
+    }
+
+}
 
 
 }) //ready function closing brackets
-
